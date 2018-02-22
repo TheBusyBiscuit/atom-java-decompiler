5d1fc8b765aea6a55747bb06754eeb2196ce64ea
TheBusyBiscuit <mrCookieSlime@gmail.com>
2018-02-22 15:26:03 +0100

    Fixed Autostart problems

--- spec/atom-java-decompiler-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomJavaDecompilerView from '../lib/atom-java-decompiler-view';
-
-describe('AtomJavaDecompilerView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
